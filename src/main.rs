ca9c00e028a283ee08e460c9faef81efedfe7d60
mikhailantoshkin <mike.antoshkin@gmail.com>
2023-06-07 19:41:21 +0900

    Move to anyhow for error handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::fmt::Display;
 use std::fs::File;
 use std::io::Read;
 use std::num::NonZeroU32;
 use std::path::PathBuf;
 use std::process;
 
+use anyhow::{anyhow, bail, Context, Result};
 use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpStream, UdpSocket};
 
@@ -24,7 +26,7 @@ struct Cli {
     #[arg(short, long, value_parser)]
     addr: String,
     /// what trasport layer protocol to use
-    #[arg(short, long, value_enum,  default_value_t = Transport::TCP)]
+    #[arg(short, long, value_enum,  default_value_t = Transport::Tcp)]
     transport: Transport,
     /// what syslog protocol to use
     #[arg(short, long, value_enum, default_value_t = Protocol::Syslog5424)]
@@ -40,20 +42,33 @@ enum Protocol {
     Syslog5424,
 }
 
+impl Display for Protocol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Protocol::Syslog3164 => "Syslog 3164",
+                Protocol::Syslog5424 => "Syslog 5424",
+            }
+        )
+    }
+}
+
 #[derive(Clone, ValueEnum)]
 enum Transport {
-    TCP,
-    UDP,
+    Tcp,
+    Udp,
 }
 
 #[inline(always)]
-fn get_syslog_msg(msg_type: &Protocol, pid: u32, msg_id: i32, body: &str) -> String {
+fn get_syslog_msg(msg_type: &Protocol, pid: u32, msg_id: i32, body: &str) -> Result<String> {
     let hostname = hostname::get()
-        .expect("Unable to get hostname!")
+        .context("Unable to get hostname!")?
         .into_string()
-        .expect("Unable to convert hostname to UTF-8, wtf is your hostname anyway?");
+        .map_err(|_| anyhow!("Unable to parse hostname to UTF-8"))?;
     let datetime = Local::now();
-    match msg_type {
+    let msg = match msg_type {
         Protocol::Syslog3164 => format!(
             "<34>{} {} spamlog[{}]: msg_id {} {}\n",
             datetime.naive_local().format("%b %e %H:%M:%S"),
@@ -70,81 +85,105 @@ fn get_syslog_msg(msg_type: &Protocol, pid: u32, msg_id: i32, body: &str) -> Str
             msg_id,
             body,
         ),
-    }
+    };
+    Ok(msg)
 }
 
-async fn spam_tcp(addr: &str, msg_type: Protocol, rate: NonZeroU32, body: &str) {
-    let mut stream = TcpStream::connect(addr).await.expect("Unable to connect!");
+async fn spam_tcp(addr: &str, msg_type: Protocol, rate: NonZeroU32, body: &str) -> Result<()> {
+    let mut stream = TcpStream::connect(addr)
+        .await
+        .context("Unable to connect to specified addres")?;
     let limiter = RateLimiter::direct(Quota::per_second(rate));
     let pid = process::id();
-    println!("Spamming {addr} over TCP");
+    println!("Spamming {addr} over TCP {msg_type}");
     for num in 0.. {
-        let msg = get_syslog_msg(&msg_type, pid, num, body);
+        let msg = get_syslog_msg(&msg_type, pid, num, body)?;
         limiter.until_ready().await;
-        stream.write_all(msg.as_bytes()).await.unwrap();
+        stream
+            .write_all(msg.as_bytes())
+            .await
+            .context("Unable to send the message!")?;
     }
+    Ok(())
 }
 
-async fn ddos_tcp(addr: &str, msg_type: Protocol, body: &str) {
-    let mut stream = TcpStream::connect(addr).await.expect("Unable to connect!");
-    let pid = process::id();
-    println!("DDoS'ing {addr} over TCP");
-    for num in 0.. {
-        let msg = get_syslog_msg(&msg_type, pid, num, body);
-        stream.write_all(msg.as_bytes()).await.unwrap();
-    }
-}
-
-async fn spam_udp(addr: &str, msg_type: Protocol, rate: NonZeroU32, body: &str) {
-    let socket = UdpSocket::bind("0.0.0.0:2345")
+async fn ddos_tcp(addr: &str, msg_type: Protocol, body: &str) -> Result<()> {
+    let mut stream = TcpStream::connect(addr)
         .await
-        .expect("Unable to bind!");
+        .context("Unable to connect to specified addres")?;
+    let pid = process::id();
+    println!("DDoS'ing {addr} over TCP {msg_type}");
+    for num in 0.. {
+        let msg = get_syslog_msg(&msg_type, pid, num, body)?;
+        stream
+            .write_all(msg.as_bytes())
+            .await
+            .context("Unable to send the message!")?;
+    }
+    Ok(())
+}
+
+async fn spam_udp(addr: &str, msg_type: Protocol, rate: NonZeroU32, body: &str) -> Result<()> {
+    let socket = UdpSocket::bind("0.0.0.0:0")
+        .await
+        .context("Unable to bind the socket")?;
     let pid = process::id();
 
     let limiter = RateLimiter::direct(Quota::per_second(rate));
-    println!("Spamming {addr} over UDP");
+    println!("Spamming {addr} over UDP {msg_type}");
     for num in 0.. {
-        let msg = get_syslog_msg(&msg_type, pid, num, body);
+        let msg = get_syslog_msg(&msg_type, pid, num, body)?;
         limiter.until_ready().await;
-        socket.send_to(msg.as_bytes(), addr).await.unwrap();
+        socket
+            .send_to(msg.as_bytes(), addr)
+            .await
+            .context("Unable to send the message!")?;
     }
+    Ok(())
 }
 
-async fn ddos_udp(addr: &str, msg_type: Protocol, body: &str) {
-    let socket = UdpSocket::bind("0.0.0.0:2345")
+async fn ddos_udp(addr: &str, msg_type: Protocol, body: &str) -> Result<()> {
+    let socket = UdpSocket::bind("0.0.0.0:0")
         .await
-        .expect("Unable to bind!");
+        .context("Unable to bind the socket")?;
     let pid = process::id();
-    println!("DDoS'ing {addr} over UDP");
+    println!("DDoS'ing {addr} over UDP with {msg_type}");
     for num in 0.. {
-        let msg = get_syslog_msg(&msg_type, pid, num, body);
-        socket.send_to(msg.as_bytes(), addr).await.unwrap();
+        let msg = get_syslog_msg(&msg_type, pid, num, body)?;
+        socket
+            .send_to(msg.as_bytes(), addr)
+            .await
+            .context("Unable to send the message!")?;
     }
+    Ok(())
 }
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let cli = Cli::parse();
     let body = cli
         .file
-        .and_then(|x| {
-            let mut file = File::open(x).unwrap();
+        .map(|x| {
+            let mut file = File::open(x).context("Unable to open file!")?;
             let mut data = String::new();
-            let bytes = file.read_to_string(&mut data).unwrap();
+            let bytes = file
+                .read_to_string(&mut data)
+                .context("Unable to read file contents!")?;
             if bytes != 0 {
-                Some(data)
+                Ok(data)
             } else {
-                None
+                bail!("File is empy!")
             }
         })
-        .unwrap_or(cli.body);
+        .unwrap_or(Ok(cli.body))?;
     match cli.transport {
-        Transport::UDP => match cli.rate {
-            Some(r) => spam_udp(&cli.addr, cli.portocol, r, &body).await,
-            None => ddos_udp(&cli.addr, cli.portocol, &body).await,
+        Transport::Udp => match cli.rate {
+            Some(r) => spam_udp(&cli.addr, cli.portocol, r, &body).await?,
+            None => ddos_udp(&cli.addr, cli.portocol, &body).await?,
         },
-        Transport::TCP => match cli.rate {
-            Some(r) => spam_tcp(&cli.addr, cli.portocol, r, &body).await,
-            None => ddos_tcp(&cli.addr, cli.portocol, &body).await,
+        Transport::Tcp => match cli.rate {
+            Some(r) => spam_tcp(&cli.addr, cli.portocol, r, &body).await?,
+            None => ddos_tcp(&cli.addr, cli.portocol, &body).await?,
         },
     }
+    Ok(())
 }
